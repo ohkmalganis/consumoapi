f5cedd536d11006d8340428f462e4deb53525d64
Lenin Aparicio <ohkmalganis@gmail.com>
2019-08-12 00:56:15 -0400

    Api Rest

--- src/app/components/apirest/apirest.component.ts
+++ src/app/components/apirest/apirest.component.ts
@@ -1,15 +1,48 @@
 import { Component, OnInit } from '@angular/core';
+import { DefaultService } from '../default/default.service';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-apirest',
   templateUrl: './apirest.component.html',
-  styleUrls: ['./apirest.component.css']
+  styleUrls: ['./apirest.component.css'],
+  providers: [DefaultService]
 })
 export class ApirestComponent implements OnInit {
 
-  constructor() { }
+  public page_title;
+  public thereis = false;
+  public info = false;
+  public data:any;
+
+  constructor(
+    private _router: Router,
+    private _defaultService: DefaultService
+  ) 
+  {
+    this.page_title = 'Operaciones del Api rest';
+  }
 
   ngOnInit() {
+    this.getData();
+  }
+
+  getData()
+  {
+    this._defaultService.getDatas().subscribe((res) => {
+      console.log(res);
+      if(res)
+      {
+        this.info = true;
+        if (res.data.length > 0) {
+          this.data = res.data;
+        }
+      }
+      
+    }, (err) => {
+      this.thereis = false;
+      console.log("Sin informacion");
+    })
   }
 
 }
--- src/app/components/default/default.service.ts
+++ src/app/components/default/default.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { GLOBAL } from '../../config';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DefaultService {
 
-  constructor() { }
+  public url: string;
+  public status: string;
+
+  constructor(
+    public _http: HttpClient
+  ) 
+  {
+    this.url = GLOBAL.url;
+  }
+
+  getDatas(): Observable<any> {
+    let headers = new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded');
+    return this._http.get(this.url + 'users', { headers: headers });
+  }
+
+
 }
