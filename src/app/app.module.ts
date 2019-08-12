5473db9c56d6411a6b2d6b109f6155e504fe53c8
Lenin Aparicio <ohkmalganis@gmail.com>
2019-08-12 07:34:25 -0400

    GraphQL

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,8 +11,9 @@ import { PostComponent } from './components/apirest/post/post.component';
 import { PutComponent } from './components/apirest/put/put.component';
 import { PatchComponent } from './components/apirest/patch/patch.component';
 import { DeleteComponent } from './components/apirest/delete/delete.component';
-import { ApolloModule } from 'apollo-angular';
-import { HttpLinkModule } from 'apollo-angular-link-http';
+import { ApolloModule, Apollo } from 'apollo-angular';
+import { HttpLinkModule, HttpLink } from 'apollo-angular-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
 
 @NgModule({
   declarations: [
@@ -39,4 +40,11 @@ import { HttpLinkModule } from 'apollo-angular-link-http';
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+  constructor(apollo: Apollo, httpLink: HttpLink) {
+    apollo.create({
+      link: httpLink.create({ uri: 'https://launchpad.graphql.com/vm8mjvrnv3' }),
+      cache: new InMemoryCache()
+    });
+  }
+}
--- src/app/components/graphql/graphql.component.ts
+++ src/app/components/graphql/graphql.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Apollo } from 'apollo-angular';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Course, Query } from '../../config';
+import gql from 'graphql-tag';
 
 @Component({
   selector: 'app-graphql',
@@ -7,9 +12,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GraphqlComponent implements OnInit {
 
-  constructor() { }
+  courses: Observable<Course[]>;
+
+  constructor(
+    private apollo: Apollo
+  ) { }
 
   ngOnInit() {
+    this.theFunction();
   }
 
+  theFunction()
+  {
+    this.courses = this.apollo.watchQuery<Query>({
+      query: gql`
+        query allCourses {
+          allCourses {
+            id
+            title
+            author
+            description
+            topic
+            url
+          }
+        }
+      `
+    })
+      .valueChanges
+      .pipe(
+        map(result => result.data.allCourses)
+      );
+  }
 }
